29960b222a2ab48fbecc05314a9a94f597b9b1a3
Sneha Agrawal <snehaagrawal@apconic.net>
2016-07-18 16:29:21 +0530

    dsq

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-function sum_of_array(){
+function sum_of_array() {
   let array = new Array();
   value = prompt("enter elements in array");
   array = value;
   let i, sum = 0;
-  if (array[i]  === array[1]){
+  if (array[i] === array[1]) {
     return array[i];
-  } 
-  for(i = 0; i <= array.length; i++){
+  }
+  for (i = 0; i <= array.length; i++) {
     sum = sum + array[i];
     console.log(sum);
   }
